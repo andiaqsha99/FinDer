478c313e1eb3a4f8dc638413f13be5fecfa45bd1
andiaqsha99 <andi77aqsha@gmail.com>
2020-09-30 11:40:56 +0700

    update BottomNavBar using NavigationUI

--- app/src/main/java/com/tugas/www/finder/MainActivity.kt
+++ app/src/main/java/com/tugas/www/finder/MainActivity.kt
@@ -2,10 +2,30 @@ package com.tugas.www.finder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation_main)
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.nav_calendar,
+            R.id.nav_chart,
+            R.id.nav_home,
+            R.id.nav_history,
+            R.id.nav_settings
+        ).build()
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNav.setupWithNavController(navController)
+
     }
 }
