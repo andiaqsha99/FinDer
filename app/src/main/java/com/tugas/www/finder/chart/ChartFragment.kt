43f9713ccb0db75f3e5a0e38806162789683285e
Milzam Hutomo <milzamhutomo.social@gmail.com>
2020-12-01 23:51:08 +0700

    Finalize Pie Chart

--- app/src/main/java/com/tugas/www/finder/chart/ChartFragment.kt
+++ app/src/main/java/com/tugas/www/finder/chart/ChartFragment.kt
@@ -5,7 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import com.anychart.AnyChart
+import com.anychart.chart.common.dataentry.DataEntry
+import com.anychart.chart.common.dataentry.ValueDataEntry
+import com.anychart.charts.Pie
 import com.tugas.www.finder.R
+import com.tugas.www.finder.home.HomeMainAdapter
+import com.tugas.www.finder.home.HomeViewModel
+import kotlinx.android.synthetic.main.fragment_chart.*
+import kotlinx.android.synthetic.main.fragment_home.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.lang.Exception
+import java.util.*
 
 
 /**
@@ -14,13 +27,11 @@ import com.tugas.www.finder.R
  * create an instance of this fragment.
  */
 class ChartFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val category = arrayOf("Income", "Expense")
+    private val number = arrayOf(0, 0)
+    private val viewModel by viewModel<HomeViewModel>()
+    private lateinit var homeMainAdapter: HomeMainAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,5 +43,39 @@ class ChartFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        try {
+            viewModel.getListNote().observe(viewLifecycleOwner, Observer{
+                homeMainAdapter.setNote(it)
+                var income = 0
+                var expense = 0
+
+                for (i in it.indices){
+                    if (it[i].type == "income"){
+                        income += it[i].amount
+                    }else{
+                        expense += it[i].amount
+                    }
+                }
+
+            })
+        }catch (e: Exception){
+            Toast.makeText(context, "No Data Found", Toast.LENGTH_SHORT)
+        }
+
+        setupPieChart()
+
+    }
+
+    fun setupPieChart(){
+        val pie: Pie = AnyChart.pie()
+        val dataEntries: MutableList<DataEntry> = ArrayList()
+
+        for(i in category.indices){
+            dataEntries.add(ValueDataEntry(category[i], number[i]))
+        }
+
+        pie.data(dataEntries)
+        any_chart_view.setChart(pie)
     }
 }
